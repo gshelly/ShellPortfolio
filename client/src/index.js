9a448a013389149b9a8c90b3238f8960d860e993
gshelly <gshelly1402@gmail.com>
2021-11-10 18:20:20 -0800

    use HashRouter instead of BrowserRouter to resolve 404 issue

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>, document.getElementById('root')
+  </HashRouter>, document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
