b34eddd14ee361c69eff31e303975f056e766bc7
gshelly <gshelly1402@gmail.com>
2021-11-15 13:21:42 -0800

    removed commented code

--- client/src/components/PageTransition/HomePageTransition.js
+++ client/src/components/PageTransition/HomePageTransition.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import styled, { keyframes } from "styled-components";
 
 const HomePageTransition = (props) => {
@@ -17,33 +17,7 @@ const HomePageTransition = (props) => {
       z-index: 999;
       height: 100vh;
     }
-    /* 10%{
-      transform: scale(1, 0.002)
-      background-color: ${props.selectedColor};
-      z-index: 999;
-      height: 100vh;}
-  35%{
-    transform: scale(0.2, 0.002);
-    background-color: ${props.selectedColor};
-      z-index: 999;
-      height: 100vh;
-    opacity: 1;}
-  50%{
-    transform: scale(0.2, 0.002);
-    background-color: ${props.selectedColor};
-      z-index: 999;
-      height: 100vh;
-    opacity: 0;}
-  80% {
-    transform :scale(1, 0.002);
-    background-color: ${props.selectedColor};
-      z-index: 999;
-      height: 100vh;}
-      100% {
-    transform :scale(1, 1);
-    background-color: ${props.selectedColor};
-      z-index: 999;
-      height: 100vh;} */
+
   `;
   const Transition1 = styled.div` 
     position: relative;
